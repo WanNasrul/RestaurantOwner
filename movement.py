1ad4ce8727afe8c87fafc1639ac0b20980a6550d
IliyaAiman <iliyaaiman@gmail.com>
2024-04-24 15:40:09 +0800

    plus

---
+++ movement.py
@@ -0,0 +1,19 @@
+import pygame, sys
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+from pathfinding.core.diagonal_movement import DiagonalMovement
+
+
+#pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280,720))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
