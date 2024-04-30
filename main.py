829847bfb70eac33019290bbc59fbac3c2715366
Fahim Fuad <fahim@Fahims-MacBook-Air.local>
2024-04-30 21:39:30 +0800

    test feature

--- main.py
+++ main.py
@@ -24,6 +24,17 @@ start_button = button.Button(515, 350, start_img, 0.5)
 credit_button = button.Button(515, 450, credit_img, 0.5)
 exit_button = button.Button(515, 550, exit_img, 0.5)
 
+# characters
+cheftest = pygame.image.load('gameasset/testchef.png').convert_alpha()
+fern_img = pygame.image.load('gameasset/fern.png').convert_alpha()
+waitertest = pygame.image.load('gameasset/testwaiter.png').convert_alpha()
+
+
+cheftest_button = button.Button(175, 65, cheftest, 0.09)
+fern_button = button.Button(508, 300, fern_img, 0.11)
+waitertest_button = button.Button(460, 65, waitertest, 0.03)
+
+
 def main_menu():
     run = True
     while run:
@@ -52,6 +63,30 @@ def game_screen():
         screen.blit(bg_game_screen, (0, 0))
 
         # game screen code here
+        if cheftest_button.draw(screen):
+            chef_menu()
+
+        waitertest_button.draw(screen)
+
+        fern_button.draw(screen)
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        pygame.display.update()
+
+def chef_menu():
+
+    chef_menu_screen = pygame.Surface((350, 350))
+    run = True
+    while run:
+        screen.blit(bg_game_screen, (0,0))
+        chef_menu_screen.fill((255, 0, 0))
+
+        screen.blit(chef_menu_screen, (100, 100))
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
