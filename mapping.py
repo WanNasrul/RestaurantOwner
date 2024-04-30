de4d329249bfae5a0dfc471c04be365213c69920
IliyaAiman <iliyaaiman@gmail.com>
2024-05-01 00:43:17 +0800

    change some mapping

--- mapping.py
+++ mapping.py
@@ -1,14 +1,11 @@
-import pygame, sys
-from pathfinding.core.grid import Grid
-from pathfinding.finder.a_star import AStarFinder
-from pathfinding.core.diagonal_movement import DiagonalMovement
+import pygame
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 
-# game setup
+# map setup
 def map():
     bg_surf = pygame.image.load('gameasset/background.png').convert()
     sprite_surf = pygame.image.load('gameasset/tablechair.png').convert_alpha()  # Load sprite with transparency
