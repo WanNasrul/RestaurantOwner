45d09aa98e6c3844e157042a931c943fed9b51cd
fahimmfuad <fahimmfuad@gmail.com>
2024-06-05 16:08:58 +0800

    debug rect

--- main.py
+++ main.py
@@ -124,7 +124,6 @@ upgrade_button5 = button.Button(1050,315, upgradebutton_img,1)
 upgrade_button6 = button.Button(1050,475, upgradebutton_img,1)
 
 # decoration ui images
-decorationuibackground_img = pygame.image.load('gameasset/decoration ui/decorationuibackground.png').convert_alpha()
 
 piano_img = pygame.image.load('gameasset/decoration ui/piano.png').convert_alpha()
 pianoui_img = pygame.image.load('gameasset/decoration ui/pianoui.png').convert_alpha()
@@ -738,7 +737,7 @@ def game_screen():
     tablechair1_rect = pygame.Rect(tablechair1X, tablechair1Y, 252, 50)
     tablechair2_rect = pygame.Rect(tablechair2X, tablechair2Y, 252, 50)
     tablechair3_rect = pygame.Rect(tablechair3X, tablechair3Y, 252, 50)
-
+    pygame.draw.rect(screen, (255, 0, 0), tablechair1_rect)
     # food rect and surf
     foodtrigger_surf = pygame.image.load('gameasset/chef ui/emptybox.png').convert_alpha()
     foodtrigger_scaled = pygame.transform.scale(foodtrigger_surf, (foodtrigger_surf.get_width() * 0.5, foodtrigger_surf.get_height() * 0.5))
